ceb4e58809a720f9c44c6e47bb8950481a0eecdc
Julius Markwei <juliusmarkwei2000@gmail.com>
2025-03-11 11:36:01 +0000

    fix: linting issue

--- src/components/product-single/reviews/reviews-list/index.tsx
+++ src/components/product-single/reviews/reviews-list/index.tsx
@@ -3,33 +3,27 @@ import Rater from "react-rater";
 import type { ReviewType } from "@/types";
 
 type ReviewsListType = {
-	reviews: ReviewType[];
+  reviews: ReviewType[];
 };
 
 const ReviewsList = ({ reviews }: ReviewsListType) => {
-	return (
-		<section className="reviews-list">
-			{reviews.map((review, index) => (
-				<div key={index} className="review-item">
-					<div className="review__avatar">
-						<img src={review.avatar} alt="avatar" />
-					</div>
+  return (
+    <section className="reviews-list">
+      {reviews.map((review, index) => (
+        <div key={index} className="review-item">
+          <div className="review__avatar">
+            <img src={review.avatar} alt="avatar" />
+          </div>
 
-					<div className="review__content">
-						<h3>{review.name}</h3>
-						<Rater
-							total={5}
-							interactive={false}
-							rating={review.punctuation}
-						/>
-						<div className="review__comment">
-							{review.description}
-						</div>
-					</div>
-				</div>
-			))}
-		</section>
-	);
+          <div className="review__content">
+            <h3>{review.name}</h3>
+            <Rater total={5} interactive={false} rating={review.punctuation} />
+            <div className="review__comment">{review.description}</div>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default ReviewsList;
--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -2,13 +2,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 // fake login
 export default (req: NextApiRequest, res: NextApiResponse) => {
-	const body = req.body;
-	const { email } = body;
-	const { password } = body;
+  const body = req.body;
+  const { email } = body;
+  const { password } = body;
 
-	if (email === "johndoe@mail.com" && password === "commerce") {
-		res.status(200).json({ status: true });
-	} else {
-		res.status(401).json({ status: false });
-	}
+  if (email === "johndoe@mail.com" && password === "commerce") {
+    res.status(200).json({ status: true });
+  } else {
+    res.status(401).json({ status: false });
+  }
 };
